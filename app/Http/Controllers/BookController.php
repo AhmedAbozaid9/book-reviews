65e62742c846b9cc81f31d67b9e96965e05281e3
Ahmed <ahmedabozaid.dev@gmail.com>
2024-11-17 21:09:43 +0200

    adds page for single book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -56,7 +56,7 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        //
+        return view('books.show', ['book' => $book->load(["reviews" => fn($query) => $query->latest()])]);
     }
 
     /**
